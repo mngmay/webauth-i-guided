49bfd35ef5ba54b620fff6776b9442958a93655a
May Ng <mngmay@gmail.com>
2019-09-16 10:44:17 -0700

    created mw

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ server.post("/api/login", (req, res) => {
     });
 });
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", validation, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -66,3 +66,24 @@ server.get("/hash", (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+/* write a middleware that will check for the username and password 
+and let the request continue to /api/users if credentials are good
+return a 401 if the credentials are invalid
+
+use the middleware to restrict access to GET /api/users endpoint
+*/
+
+function validation(req, res, next) {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    });
+}
